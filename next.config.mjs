77f49fbfb0d4e43d9581fcd9941d75b1e5f4628a
AnkitRastogi3 <ankit.rastogi@100ms.live>
2024-07-26 16:43:48 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["api.slingacademy.com"],
   },
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Optional, if you have issues with image optimization
+  },
 };
 
 export default nextConfig;
